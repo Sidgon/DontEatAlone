cc193bb225f2a101e851f6128eba65ba20603ca5
Daniel Steinegger <steinegger.daniel@gmail.com>
2018-10-04 16:24:11 +0200

    Create UserProfile and UserEdit

---
+++ app/src/main/java/ch/mse/dea/donteatalone/edit_user_profile.java
@@ -0,0 +1,13 @@
+package ch.mse.dea.donteatalone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class edit_user_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_profile);
+    }
+}
