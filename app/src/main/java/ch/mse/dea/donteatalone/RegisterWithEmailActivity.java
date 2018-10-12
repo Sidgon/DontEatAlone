1fa70637f209bb02f64318e2d1ec9924b2eadc05
Sidgon <david.gsponer95@hotmail.com>
2018-10-12 12:17:41 +0200

    added registration and Firebase Auth

--- app/src/main/java/ch/mse/dea/donteatalone/RegisterWithEmailActivity.java
+++ app/src/main/java/ch/mse/dea/donteatalone/RegisterWithEmailActivity.java
@@ -1,17 +1,25 @@
 package ch.mse.dea.donteatalone;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import org.w3c.dom.Text;
 
 public class RegisterWithEmailActivity extends AppCompatActivity implements
         View.OnClickListener {
+
+    private static final String TAG = "EmailPassword";
 
     private EditText mUsernameTextField;
     private EditText mEmailTextField;
@@ -25,6 +33,7 @@ public class RegisterWithEmailActivity extends AppCompatActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_register_with_email);
         //set onclicklistener on sign in button
         findViewById(R.id.registerSignUpButton).setOnClickListener(this);
@@ -58,17 +67,39 @@ public class RegisterWithEmailActivity extends AppCompatActivity implements
         }
 
         //create user for firebase auth
+        signUpFirebaseAuthUser(email, password);
 
         //create user in realtime db with all attributes
+        //signUpFirebaseRealTimeDBUser(username, email, firstname, lastname, password);
 
     }
 
     public void signUpFirebaseAuthUser(String email, String password){
 
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //-> change intent here
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterWithEmailActivity.this, "Authentication failed." +
+                                    "please contact an administrator",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
     }
 
-    public void signUpFirebaseRealTimeDBUser(String Username, String email, String firstname,
+    public void signUpFirebaseRealTimeDBUser(String username, String email, String firstname,
                                 String lastname, String password){
+
 
     }
 
